2fe889f02cd1729b4763b425e613f224bf4f3f45
Balaji-Nandure <balajirn.2016@gmail.com>
2023-05-10 19:02:50 +0530

    checkIfNumberIsPowerOfTwo

--- 2. Bit Magic/checkIfNumberIsPowerOfTwo.java
+++ 2. Bit Magic/checkIfNumberIsPowerOfTwo.java
@@ -1,3 +1,10 @@
 public class checkIfNumberIsPowerOfTwo {
+    // idea (for 2^n) n & (n-1) == 0;
+    public static boolean isTwoPow(int n) {
+        return (n & (n - 1)) == 0;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(isTwoPow(3));
+    }
 }
