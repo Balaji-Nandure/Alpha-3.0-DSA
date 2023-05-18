ce96dfbc54b91d68d791cebe949504e0fb8a86d6
Balaji-Nandure <balajirn.2016@gmail.com>
2023-05-18 14:53:34 +0530

    removeDuplicateInString

--- 3. Recursion/removeDuplicateInString.java
+++ 3. Recursion/removeDuplicateInString.java
@@ -1,11 +1,30 @@
+import javax.swing.Spring;
+
 public class removeDuplicateInString {
-    public static void name() {
+    public static void removeDuplicate(String str, int idx, StringBuilder newStr, boolean map[]) {
         // chars form a to z only (small)
         // a-a=0, b-a=1, c-a=2....z-a=25
 
+        // basecase
+        if (idx == str.length()) {
+            System.out.println(newStr);
+            return;
+        }
+
+        // work
+        char currentChar = str.charAt(idx);
+        if (map[currentChar - 'a'] == true) {
+            // if duplicate is found
+            removeDuplicate(str, idx + 1, newStr, map);
+        } else {
+            // if duplicate is not found
+            map[currentChar - 'a'] = true;
+            removeDuplicate(str, idx + 1, newStr.append(currentChar), map);
+        }
     }
 
     public static void main(String[] args) {
-
+        String str = "appnaacollege";
+        removeDuplicate(str, 0, new StringBuilder(""), new boolean[26]);
     }
 }
