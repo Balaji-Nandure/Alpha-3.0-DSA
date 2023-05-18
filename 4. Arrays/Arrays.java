df0eb85fd8b737ecef8470a88f7f79b50c5fb241
Balaji-Nandure <balajirn.2016@gmail.com>
2023-05-18 21:36:20 +0530

    reverse

--- 4. Arrays/Arrays.java
+++ 4. Arrays/Arrays.java
@@ -1,4 +1,11 @@
 public class Arrays {
+    // print array
+    public static void printarray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + ", ");
+        }
+    }
+
     // tc = O(n)
     public static int linearSearch(int numbers[], int key) {
         for (int i = 0; i < numbers.length; i++) {
@@ -31,6 +38,7 @@ public class Arrays {
         return smallest;
     }
 
+    // tc = Olog(n)
     public static int binarySearch(int arr[], int key) {
         int start = 0, end = arr.length - 1;
 
@@ -45,15 +53,34 @@ public class Arrays {
                 end = mid - 1;
             }
         }
-
         return -1;
+    }
+
+    public static void reverse(int arr[]) {
+        // No extra space
+        // two pointer approach
+        int first = 0, last = arr.length - 1;
+        while (first < last) {
+            // swap
+            int temp = arr[first];
+            arr[first] = arr[last];
+            arr[last] = temp;
+
+            first++;
+            last--;
+        }
+
     }
 
     public static void main(String[] args) {
         int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+        // printarray(numbers);
         // System.out.println(linearSearch(numbers, 15));
         // System.out.println(largestNum(numbers));
         // System.out.println(smallestNum(numbers));
-        System.out.println(binarySearch(numbers, 11));
+        // System.out.println(binarySearch(numbers, 11));
+
+        reverse(numbers);
+        printarray(numbers);
     }
 }
