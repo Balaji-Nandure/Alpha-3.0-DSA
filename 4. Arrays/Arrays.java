acbc2c58e785f953b76db29fc75e1aaeff7e588f
Balaji-Nandure <balajirn.2016@gmail.com>
2023-05-19 00:25:11 +0530

    printSubarrays

--- 4. Arrays/Arrays.java
+++ 4. Arrays/Arrays.java
@@ -86,6 +86,21 @@ public class Arrays {
 
     }
 
+    // Print subarrays (continuous)
+    // tc=o(n^3)
+    public static void printSubarrays(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            int start = i;
+            for (int j = start; j < arr.length; j++) {
+                int end = j;
+                for (int k = start; k <= end; k++) {
+                    System.out.print(arr[k] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
         // printarray(numbers);
@@ -97,6 +112,7 @@ public class Arrays {
         // reverse(numbers);
         // printarray(numbers);
 
-        printPairs(numbers);
+        // printPairs(numbers);
+        printSubarrays(numbers);
     }
 }
