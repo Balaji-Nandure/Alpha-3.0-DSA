fc321b521f5e95fc939085f80422f70671be0a6d
Balaji-Nandure <balajirn.2016@gmail.com>
2023-05-18 12:03:11 +0530

    remove duplicate

---
+++ 3. Recursion/removeDuplicateInString.java
@@ -0,0 +1,11 @@
+public class removeDuplicateInString {
+    public static void name() {
+        // chars form a to z only (small)
+        // a-a=0, b-a=1, c-a=2....z-a=25
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
