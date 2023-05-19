c11965f8e424f5d3a50f2656bb87c5ac0e5336fb
Balaji-Nandure <balajirn.2016@gmail.com>
2023-05-19 08:31:17 +0530

    maxSubarraySum2

--- 4. Arrays/Arrays.java
+++ 4. Arrays/Arrays.java
@@ -126,6 +126,31 @@ public class Arrays {
     }
 
     // prefix sum approach
+    // tc=o(n^2)
+    public static void maxSubarraySum2(int arr[]) {
+        int currSum = 0;
+        int maxSum = arr[0];
+        int prefix[] = new int[arr.length];
+
+        prefix[0] = arr[0];
+        // to calculate prefix
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int start = i;
+            for (int j = start; j < arr.length; j++) {
+                int end = j;
+                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
+                if (maxSum < currSum) {
+                    maxSum = currSum;
+                }
+            }
+        }
+
+        System.out.println("maxSum = " + maxSum);
+    }
 
     public static void main(String[] args) {
         int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
@@ -141,6 +166,7 @@ public class Arrays {
 
         // printPairs(numbers);
         // printSubarrays(numbers);
-        maxSubarraySum(arr);
+        // maxSubarraySum(arr);
+        maxSubarraySum2(arr);
     }
 }
